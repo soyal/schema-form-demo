165f11c5a3ddccecd63039436c55218580394685
Soyal <345723940@qq.com>
2021-07-04 21:49:24 +0800

    save

--- src/typings/form.d.ts
+++ src/typings/form.d.ts
@@ -4,15 +4,14 @@ export interface FormProps<FormDataType> {
   schema: FormSchema<FormDataType>;
   formData?: FormDataTypem;
   onSubmit: (formData: FormDataType) => void;
-  // onChange: (formData: FormDataType) => void;
 }
 
 export interface FormItemProps<ValueType> {
-  onChange: (nValue: ValueType) => void; // required
-  value?: ValueType; // required
+  onChange: (nValue: ValueType) => void; // required to implement
+  value?: ValueType; // required to implement
   label?: string;
   field?: string;
-  disabled?: boolean; // required
+  disabled?: boolean; // required to implement
   onBlur?: (value: ValueType) => void;
   onFocus?: (value: ValueType) => void;
 }
