08be0abb3a28a10d0c885ca92d62792b86f6b2d9
soyal <xiongxiao9958@gmail.com>
2021-07-13 20:29:12 +0800

    add: public path

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,5 +4,13 @@ module.exports = {
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
-  }
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.output.publicPath = '/musician-schema-form/';
+    return config;
+  },
+  managerWebpack: async (config) => {
+    config.output.publicPath = '/musician-schema-form/';
+    return config;
+  },
 };
