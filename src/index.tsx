9a56aea405b1d57d4aa215d5b6db9cdfa7f173e5
soyal <xiongxiao9958@gmail.com>
2021-07-13 19:55:33 +0800

    fix: 代码错误

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export { default as default } from './SchemaForm';
-// export { default as useSchemaForm } from './useSchemaForm';
-// export * from './typings/schema';
-// export * from './typings/form';
+export { default as useSchemaForm } from './useSchemaForm';
+export * from './typings/schema';
+export * from './typings/form';
