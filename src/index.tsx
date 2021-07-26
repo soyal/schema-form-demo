d15ff0ee07de115587a2aa9dd58890ce603200fe
Soyal <345723940@qq.com>
2021-07-26 16:06:14 +0800

    feat: export SchemaFormInstance

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export { default as default } from './SchemaForm';
-export { default as useSchemaForm } from './useSchemaForm';
+export { default as useSchemaForm, SchemaFormInstance } from './useSchemaForm';
 export * from './typings/schema';
 export * from './typings/form';
